7d2322a8b48a86067e08a14170d84e82090e64af
ghkdtlwns987 <ghkdtlwns987@naver.com>
2023-02-01 08:57:53 +0900

    Test

---
+++ src/main/java/com/ipTest/demo/MhApplication.java
@@ -0,0 +1,28 @@
+package com.ipTest.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@SpringBootApplication
+public class MhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MhApplication.class, args);
+	}
+	@GetMapping("/ip")
+	public Map<String, String> ip(HttpServletRequest request) throws UnknownHostException {
+		return new HashMap<>() {{
+			put("hostIp", InetAddress.getLocalHost().getHostAddress());
+			put("accessIp", request.getHeader("x-forwarded-for"));
+		}};
+	}
+}
---
+++ src/test/java/com/ipTest/demo/MhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipTest.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
