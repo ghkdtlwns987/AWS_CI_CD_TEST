fb6b1917a21d7df1cfd07e8fb771213ca025b47d
ghkdtlwns987 <54488922+ghkdtlwns987@users.noreply.github.com>
2023-02-02 15:29:03 +0900

    Update deploy.sh

--- codedeploy/deploy.sh
+++ codedeploy/deploy.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-# 만약 현재 위치한 디렉터리에 .jar 파일이 있다면 .jar 파일 삭제
-FILE=*.jar
-if [ -e $FILE ]; then
-  rm -rf *.jar
-fi
-
-nohup java -jar -Dserver.port=8082 /home/ubuntu/application.jar 1> /dev/null 2>&1 &
+nohup java -jar -Dserver.port=8082 /home/ubuntu/MH-0.0.1-SNAPSHOT.jar 1> /dev/null 2>&1 &
 sleep 30s
